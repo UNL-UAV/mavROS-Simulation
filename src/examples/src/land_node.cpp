f2425a270179eae49b0214c51486bf0b3d2d46cc
Gerson Uriarte <gersongru@huskers.unl.edu>
2020-08-17 18:46:07 -0500

    Added file and created the header.

---
+++ src/examples/src/land_node.cpp
@@ -0,0 +1,8 @@
+/**
+ * ROS node that was created by Gerson Uriarte
+ * 
+ * uses mavROS to send a land the drone
+ * 
+ * Made specifically for simulation, but should work on an actual drone
+ * with a companion computer
+ * /
