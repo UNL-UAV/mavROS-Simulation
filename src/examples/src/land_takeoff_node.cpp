202782c1e035b5567676c3af950ea0bf0ea5a8d9
Gerson Uriarte <gersongru@huskers.unl.edu>
2020-09-04 18:52:17 -0500

    Added land_takeoff example to the examples project.

---
+++ src/examples/src/land_takeoff_node.cpp
@@ -0,0 +1,105 @@
+#include <ros/ros.h>
+#include <geometry_msgs/PoseStamped.h>
+#include <mavros_msgs/CommandBool.h>
+#include <mavros_msgs/SetMode.h>
+#include <mavros_msgs/State.h>
+
+mavros_msgs::State current_state;
+void state_cb(const mavros_msgs::State::ConstPtr& msg){
+    current_state = *msg;
+}
+
+ros::Time init_time;
+
+geometry_msgs::PoseStamped takeoff_land(const ros::Duration &cur_time){
+    geometry_msgs::PoseStamped next_pos;
+    float t = cur_time.toSec();
+    float mod_t = std::fmod(t, 30);
+    float xpos, ypos, zpos;
+    if(mod_t < 15){
+        xpos = 0;
+        ypos = 0;
+        zpos = mod_t/15;
+    }
+    else if(mod_t < 30){
+        xpos = 0;
+        ypos = 0;
+        zpos = 1 - (mod_t/15);
+    }
+    next_pos.pose.position.x = xpos;
+    next_pos.pose.position.y = ypos;
+    next_pos.pose.position.z = zpos;
+    return next_pos;
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "offb_node");
+    ros::NodeHandle nh;
+
+    ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>
+            ("mavros/state", 10, state_cb);
+    ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>
+            ("mavros/setpoint_position/local", 10);
+    ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>
+            ("mavros/cmd/arming");
+    ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>
+            ("mavros/set_mode");
+
+    //the setpoint publishing rate MUST be faster than 2Hz
+    ros::Rate rate(20.0);
+
+    // wait for FCU connection
+    while(ros::ok() && !current_state.connected){
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    geometry_msgs::PoseStamped pose;
+    pose.pose.position.x = 0;
+    pose.pose.position.y = 0;
+    pose.pose.position.z = 0;
+
+    //send a few setpoints before starting
+    for(int i = 10; ros::ok() && i > 0; --i){
+        local_pos_pub.publish(pose);
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    mavros_msgs::SetMode offb_set_mode;
+    offb_set_mode.request.custom_mode = "OFFBOARD";
+
+    mavros_msgs::CommandBool arm_cmd;
+    arm_cmd.request.value = true;
+
+    ros::Time last_request = ros::Time::now();
+    init_time = ros::Time::now();
+
+    while(ros::ok()){
+        if( current_state.mode != "OFFBOARD" &&
+            (ros::Time::now() - last_request > ros::Duration(5.0))){
+            if( set_mode_client.call(offb_set_mode) &&
+                offb_set_mode.response.mode_sent){
+                ROS_INFO("Offboard enabled");
+            }
+            last_request = ros::Time::now();
+        } else {
+            if( !current_state.armed &&
+                (ros::Time::now() - last_request > ros::Duration(5.0))){
+                if( arming_client.call(arm_cmd) &&
+                    arm_cmd.response.success){
+                    ROS_INFO("Vehicle armed");
+                }
+                last_request = ros::Time::now();
+            }
+        }
+
+        local_pos_pub.publish(takeoff_land(ros::Time::now() - init_time));
+
+        ros::spinOnce();
+        rate.sleep();
+    }
+
+    return 0;
+}
